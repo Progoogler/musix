3d234cae12a41a029e6c17dea1af3540dcf84101
Andy Li <andyzanli@gmail.com>
2017-10-26 01:04:10 -0700

    Render music sheet demo view

---
+++ src/components/MusicSheet.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="sheet">
+    <p class="name">{{ sheetInfo }}</p>
+    <img src="../assets/piano-sheets/chopin-nocturne-music-sheet.jpg" alt="The music sheet for Chopin's Nocture op.9 No.2">
+    <!-- <h2>Essential Links</h2>
+    <ul>
+      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
+      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
+      <li><a href="http://chat.vuejs.org/" target="_blank" rel="noopener">Vue Community Chat</a></li>
+      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
+      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank" rel="noopener">Docs for This Template</a></li>
+    </ul>
+    <h2>Ecosystem</h2>
+    <ul>
+      <li><a href="http://router.vuejs.org/" target="_blank" rel="noopener">vue-router</a></li>
+      <li><a href="http://vuex.vuejs.org/" target="_blank" rel="noopener">vuex</a></li>
+      <li><a href="http://vue-loader.vuejs.org/" target="_blank" rel="noopener">vue-loader</a></li>
+      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
+    </ul> -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MusicSheet',
+  data() {
+    return {
+      sheetInfo: 'Chopin',
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.name {
+  font-size: 4em;
+  font-weight: bold;
+}
+</style>
+1
